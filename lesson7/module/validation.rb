a684ed35c8426ea1cf44b13c1f020158f4fac4ca
Tavbulatov <alikhantam26@gmail.com>
2022-04-19 03:02:43 +0300

    corrected

--- lesson7/module/validation.rb
+++ lesson7/module/validation.rb
@@ -21,9 +21,9 @@ module Validation
 
   module InstanceMethods
     def validate!
-      self.class.validations.each do |name|
-        var_name = instance_variable_get("@#{name[:name]}")
-        send("valid_#{name[:type]}", var_name, *name[:args])
+      self.class.validations.each do |validation|
+        var_name = instance_variable_get("@#{validation[:name]}")
+        send("valid_#{validation[:type]}", var_name, *validation[:args])
       end
     end
 
@@ -34,17 +34,16 @@ module Validation
       false
     end
 
-    def valid_presence(value)
+    def validate_presence(value)
       raise 'Значение не должно быть пустым' if value.nil? || value.empty?
     end
 
-    def valid_format(value, format)
+    def validate_format (value, format)
       raise 'Значение не соответствует формату' if value !~ format
     end
 
-    # Осма это исключение в любом случае выбрасывает, не понял почему
-    def valid_type(value, attribute_class)
-      # raise 'Объект не соответствует классу' unless attribute_class.class == value
+    def validate_type(value, attribute_class)
+      raise 'Объект не соответствует классу' unless value == attribute_class.class
     end
   end
 end
